ad6116596450df6831353ab39561ac7c3006cc25
Shujie Ying <ying.sh@northeastern.edu>
2024-04-26 12:26:43 -0700

    update mongodb connection

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8000;
 const app = express();
 
 // connect to db
-mongoose.connect(process.env.MONGO_URI, {})
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true })
         .then(() => console.log("DB Connected"))
         .catch((err) => console.log("DB Connection Error => ", err));
 
